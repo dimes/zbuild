365e7c19d4ea88a8747346dc50b8a5610daa5c15
Dimes <mgale4@binghamton.edu>
2017-12-10 11:43:42 -0500

    Add path parameter to pathfinder

--- cli/pathfinder/pathfinder.go
+++ cli/pathfinder/pathfinder.go
@@ -5,18 +5,30 @@ import (
 	"strings"
 
 	"github.com/dimes/zbuild/buildlog"
+	"github.com/dimes/zbuild/cli/argv"
 	"github.com/dimes/zbuild/local"
 )
 
 func main() {
-	workingDir, err := os.Getwd()
-	if err != nil {
-		buildlog.Fatalf("Error getting working directory: %+v", err)
+	argSet := argv.NewArgSet()
+	var file string
+	argSet.ExpectString(&file, "file", "", "the file to get the path for")
+	argSet.Parse(os.Args[1:])
+
+	if file == "" {
+		workingDir, err := os.Getwd()
+		if err != nil {
+			buildlog.Fatalf("Error getting working directory: %+v", err)
+		}
+		file = workingDir
 	}
 
-	path, err := local.GetBuildpath(workingDir, local.CompileDependencyResolver)
+	// TODO: Need to determine if compile or test dependencies should be used
+
+	buildlog.Debugf("Getting build path for %s", file)
+	path, err := local.GetBuildpath(file, local.CompileDependencyResolver)
 	if err != nil {
-		buildlog.Fatalf("Error getting build path for %s: %+v", workingDir, err)
+		buildlog.Fatalf("Error getting build path for %s: %+v", file, err)
 	}
 
 	buildlog.Outputf(strings.Join(path, string(os.PathListSeparator)))
